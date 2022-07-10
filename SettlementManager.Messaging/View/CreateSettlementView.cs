568e6b4ba7d5bd011f3bf4a0bdbd1c745c3ffbdf
Marko <marko.pogacic@king-ict.hr>
2022-07-10 20:42:19 +0200

    Added working create settlement modal

--- SettlementManager.Messaging/View/CreateSettlementView.cs
+++ SettlementManager.Messaging/View/CreateSettlementView.cs
@@ -1,11 +1,17 @@
-﻿namespace SettlementManager.Messaging.View
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SettlementManager.Messaging.View
 {
 	public class CreateSettlementView
 	{
+		[Required(ErrorMessage = "Name is required")]
 		public string Name { get; set; }
 
+		[Required(ErrorMessage = "PostalCode is required")]
 		public string PostalCode { get; set; }
 
+		[Required(ErrorMessage = "Country is required")]
+		[Range(1,1000, ErrorMessage = "Enter a valid Country")]
 		public long CountryId { get; set; }
 	}
 }
